4c76b212856e08e11ab3cd92fdf0058124663483
Amit Hasan Sadhin <you@example.com>
2021-12-13 19:51:54 +0800

    Add fising on userSchema file

--- models/user.js
+++ models/user.js
@@ -1,42 +1,38 @@
-const mongoose = require('mongoose');
-const crypto = require('crypto');
-const { v1: uuidv1 } = require('uuid');
+const mongoose = require("mongoose");
+const crypto = require("crypto");
+const { v1: uuidv1 } = require("uuid");
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        trim: true,
-        required: true,
-        maxLength: 32
+      type: String,
+      trim: true,
+      required: true,
+      maxLength: 32,
     },
     email: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
     },
     password: {
-        type: String,
-        required: true,
-
-    },
-    about: {
-        type: String,
-        trim: true
+      type: String,
+      required: true,
     },
 
+    //change
 
     role: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     history: {
-        type: Array,
-        default: []
-
-    }
-},
-    { timestamps: true }
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
 );
 
 // userSchema.methods.generateJWT = function () {
@@ -73,4 +69,4 @@ const userSchema = new mongoose.Schema({
 //     }
 // };
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
