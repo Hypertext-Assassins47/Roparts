d511ae2bb432a1130bc9b88f56259975b46b8520
Amit Hasan Sadhin <you@example.com>
2021-12-13 20:01:51 +0800

    Validator Length settings

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+//Backend routing of the website
+const {
+  signup,
+  signin,
+  signout,
+  requireSignin,
+} = require("../controllers/auth");
+const { userSignupValidator } = require("../validator");
 
-const { signup, signin, signout, requireSignin } = require("../controllers/auth")
-const { userSignupValidator } = require("../validator")
-
-router.post('/signup', userSignupValidator, signup);
-router.post('/signin', signin);
-router.get('/signout', signout);
+router.post("/signup", userSignupValidator, signup);
+router.post("/signin", signin);
+router.get("/signout", signout);
 
 router.get("/hello", requireSignin, (req, res) => {
-    res.send("Hello there!");
-})
+  res.send("Hello there!");
+});
 module.exports = router;
